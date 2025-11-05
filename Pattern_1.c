8bba3af870e153086b851cc4052af5e6fc2ff8be
Md. Abid Hasan Roni Bokshi <57047094+Roni95@users.noreply.github.com>
2025-11-05 12:16:45 +0600

    Create Pattern_1.c

---
+++ Pattern_1.c
@@ -0,0 +1,21 @@
+//Print the following output for input = 5 
+//  1
+//  22
+//  333
+//  4444
+//  55555
+
+
+#include <stdio.h>
+int main(){
+    int i,j,n;
+    printf("Enter the number of N : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
